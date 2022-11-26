fa16b5e8388a3a2b1a4532fc2ce439ddf8d024bd
Elias Lima <sci.eliaslima@gmail.com>
2022-11-26 20:52:46 -0300

    Created user container for the new gofinances app

---
+++ App.tsx
@@ -0,0 +1,45 @@
+//importação de componentes de biliotecas
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from 'styled-components'
+import * as SplashScreen from 'expo-splash-screen';
+
+import {
+  useFonts, // hooks para carregar fontes
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
+
+//importação de componentes criados
+import theme from './src/global/styles/theme'
+import { Dashboard } from './src/screens/Dashboard'
+
+
+export default function App() {
+  // carrega as fontes antes de iniciar o aplicativo
+  const [fontsLoaded] = useFonts({  // useFonts retorna verdadeiro ou false 
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if(!fontsLoaded){
+    
+    SplashScreen.preventAutoHideAsync();
+  }
+  
+  else{
+    SplashScreen.hideAsync();
+    return (
+      <ThemeProvider theme={theme}>
+        <Dashboard/>
+      </ThemeProvider>
+       
+   );
+  }
+
+  
+}
+
+
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import{
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon
+
+} from './styles'
+
+
+export function Dashboard(){
+    return (
+        <Container>
+            <Header> 
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/70176310?v=4'}}/>
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Elias</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power"/>
+                </UserWrapper>
+            </Header>
+        </Container>
+
+    )
+}
