6ca9707323c6aa0cc867d3052635203ffde66788
Elias Lima <sci.eliaslima@gmail.com>
2023-01-05 13:46:13 -0300

    Added transaction filtering by specific date

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -6,6 +6,8 @@ import { useFocusEffect } from "@react-navigation/native";
 import { VictoryPie } from "victory-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useBottomTabBarHeight} from '@react-navigation/bottom-tabs';
+import { addMonths, subMonths, format } from "date-fns";
+import { ptBR } from 'date-fns/locale'
 import { ScrollView } from "react-native";
 
 
@@ -22,15 +24,31 @@ import {
 } from "./styles";
 
 export function Resume(){
-
+    const [selectedDate, setSelectedDate] = useState( new Date());
     const [totalByCategories, setTotalByCategories] = useState([]);
+
+
+    function handleDateChange(action: 'next' | 'prev'){
+        if(action === 'next'){
+            setSelectedDate(addMonths(selectedDate, 1))
+        }else{
+            setSelectedDate(subMonths(selectedDate, 1))
+        }
+    }
+
 
     async function getTransactionSummary(){
         const dataKey = '@gofinances:transactions';
         const response = await AsyncStorage.getItem(dataKey);
         const transactions = response ? JSON.parse(response): [];
-        const expensives = transactions.filter(expensive => expensive.type === 'down')  
-        
+
+        const expensives = transactions
+        .filter(expensive => 
+            expensive.type === 'down' && 
+            new Date(expensive.date).getMonth() === selectedDate.getMonth() &&
+            new Date(expensive.date).getFullYear() === selectedDate.getFullYear()
+        );
+
         const expensivesTotal = expensives.reduce(( accumulator, expensive) => {
             return accumulator + expensive.amount;
         },0);
@@ -59,7 +77,7 @@ export function Resume(){
 
     useFocusEffect( useCallback(() => { 
         getTransactionSummary();
-    },[]));
+    },[selectedDate]));
 
     return(
         <Container>
@@ -67,11 +85,11 @@ export function Resume(){
                 <Title>Resumo por categoria</Title>
             </Header>
             <MonthSelect>
-                <SelectButton>
+                <SelectButton onPress={() => handleDateChange('prev')}>
                     <SelectIcon name="chevron-left"/>
                 </SelectButton>
-                <Month>maio, 2020</Month>
-                <SelectButton>
+                <Month>{ format(selectedDate,'MMMM, yyyy', {locale: ptBR})}</Month>
+                <SelectButton onPress={() => handleDateChange('next')}>
                     <SelectIcon name="chevron-right"/>
                 </SelectButton>
             </MonthSelect>
