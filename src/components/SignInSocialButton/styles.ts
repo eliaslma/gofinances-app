ea4e104cd00bd1cd63613b79024db5275e1470bf
Elias Lima <sci.eliaslima@gmail.com>
2023-01-06 20:25:08 -0300

    feat: Create signin button

---
+++ src/components/SignInSocialButton/styles.ts
@@ -0,0 +1,26 @@
+import { RectButton } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Button = styled(RectButton)`
+    flex-direction: row;
+    align-items: center;
+    background-color: ${({ theme }) => theme.colors.shape};
+    border-radius: 5px;
+    margin-bottom: 16px;
+`;
+
+export const ImageContainer = styled.View`
+    align-items: center;
+    border-color: ${({theme}) => theme.colors.background};
+    border-right-width: 1px;
+    height: 100%;
+    padding: ${RFValue(16)}px;
+`;
+
+export const Text = styled.Text`
+    flex: 1;
+    font-family: ${({ theme }) => theme.fonts.medium};
+    color: ${({ theme }) => theme.colors.title};
+    text-align: center;
+`;
